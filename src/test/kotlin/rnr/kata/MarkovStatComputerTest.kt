a58228495c2df3a30b6c1dfc51830e0cc5d84a7f
MaximeRnR <maxime.reynier@vidal.fr>
2018-10-16 14:30:54 +0200

    can compute stats for sentences that don't have same word multiple times :fire: :fire:

--- src/test/kotlin/rnr/kata/MarkovStatComputerTest.kt
+++ src/test/kotlin/rnr/kata/MarkovStatComputerTest.kt
@@ -21,28 +21,46 @@ class MarkovStatComputerTest {
     }
 
     @Test
-    fun `le chat return map  that has key le with chat , 1 for value and key chat with empty value`() {
+    fun `le chat return map that has key le with chat , 1 for value and key chat with empty value`() {
         val computeMarkovStat = "le chat".computeMarkovStat()
         assertThat(computeMarkovStat["le"]).containsExactly(WordStat("chat", 1))
         assertThat(computeMarkovStat["chat"]).isEmpty();
     }
 
     @Test
-    fun `le chien return map  that has key le with chien , 1 for value and key chien with empty value`() {
+    fun `le chien return map that has key le with chien , 1 for value and key chien with empty value`() {
         val computeMarkovStat = "le chien".computeMarkovStat()
         assertThat(computeMarkovStat["le"]).containsExactly(WordStat("chien", 1))
         assertThat(computeMarkovStat["chien"]).isEmpty();
+    }
+
+    @Test
+    fun `le chien est cool return expected stats `() {
+        val computeMarkovStat = "le chien est cool".computeMarkovStat()
+        assertThat(computeMarkovStat["le"]).containsExactly(WordStat("chien", 1))
+        assertThat(computeMarkovStat["chien"]).containsExactly(WordStat("est", 1))
+        assertThat(computeMarkovStat["est"]).containsExactly(WordStat("cool", 1))
+        assertThat(computeMarkovStat["cool"]).isEmpty();
+    }
+
+    @Test
+    fun `le chat est nul return expected stats `() {
+        val computeMarkovStat = "le chat est nul".computeMarkovStat()
+        assertThat(computeMarkovStat["le"]).containsExactly(WordStat("chat", 1))
+        assertThat(computeMarkovStat["chat"]).containsExactly(WordStat("est", 1))
+        assertThat(computeMarkovStat["est"]).containsExactly(WordStat("nul", 1))
+        assertThat(computeMarkovStat["nul"]).isEmpty();
     }
 }
 
 private fun String.computeMarkovStat(): MutableMap<String, List<WordStat>> {
     val markovStats = mutableMapOf<String, List<WordStat>>()
 
+    val words = this.split(" ")
+
     if (this.isEmpty()) {
         return markovStats
     }
-
-    val words = this.split(" ")
 
     if(words.size == 1){
         markovStats[words[0]] = emptyList()
@@ -51,7 +69,11 @@ private fun String.computeMarkovStat(): MutableMap<String, List<WordStat>> {
 
     markovStats[words[0]] = listOf(WordStat(words[1], 1))
     markovStats[words[1]] = emptyList()
-    return markovStats
+
+    val newSentence = this.replace(words[0]+" ", "");
+    markovStats.putAll(newSentence.computeMarkovStat());
+
+    return markovStats;
 }
 
 data class WordStat(val word: String, val stat: Int)
